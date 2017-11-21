87911a1195dbcea89bee52024c1197f02b5f50f2
Marten Sytema <marten@sytematic.nl>
2017-11-21 09:57:59 +0100

    chore

--- src/cljs/re_datagrid/events.cljs
+++ src/cljs/re_datagrid/events.cljs
@@ -52,12 +52,14 @@
 (rf/reg-event-db
  :datagrid/update-options
  (fn [db [_ o]]
+   (debug "updating options")
    (assoc-in db [:datagrid/data grid-id :options] o)))
 
 
 (rf/reg-event-db
  :datagrid/update-fields
  (fn [db [_ o]]
+   (debug "updating fields")
    (assoc-in db [:datagrid/data grid-id :fields] o)))
 
 
