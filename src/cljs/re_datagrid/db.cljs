438d64b139180afb5b275bac056ea5dcfc90d1e3
Ruud Reus <ruud@reus.dev>
2024-10-02 11:23:34 +0200

    brevity

--- src/cljs/re_datagrid/db.cljs
+++ src/cljs/re_datagrid/db.cljs
@@ -1,8 +1,4 @@
 (ns re-datagrid.db
-  (:require [alandipert.storage-atom :refer [local-storage]]
-            [taoensso.timbre :as timbre
-             :refer-macros (log  trace  debug  info  warn  error  fatal  report
-                                 logf tracef debugf infof warnf errorf fatalf reportf
-                                 spy get-env log-env)]))
+  (:require [alandipert.storage-atom :refer [local-storage]]))
 
 (def local-db (local-storage (atom {}) :re-datagrid))
